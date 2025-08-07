e1315a1b7a19e9f0a4e98a0b7e6ee821b88676f0
Pablo de la Garza <pdv_88@hotmail.com>
2025-08-07 14:42:40 -0600

    add renders

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -2,6 +2,7 @@ import { PhotoProvider, PhotoView } from 'react-photo-view';
 import 'react-photo-view/dist/react-photo-view.css';
 import img1 from '../assets/gallery/img1.webp';
 import img2 from '../assets/gallery/img2.webp';
+import img3 from '../assets/gallery/img3.webp';
 import img4 from '../assets/gallery/img4.webp';
 import img5 from '../assets/gallery/img5.webp';
 import img6 from '../assets/gallery/img6.webp';
@@ -9,13 +10,12 @@ import img7 from '../assets/gallery/img7.webp';
 import img9 from '../assets/gallery/img9.webp';
 import img10 from '../assets/gallery/img10.webp';
 import img11 from '../assets/gallery/img11.webp';
-import img12 from '../assets/gallery/img12.webp';
 import img13 from '../assets/gallery/img13.webp';
 import img15 from '../assets/gallery/img15.webp';
 
 const Gallery = () => {
   
-    const images = [img1, img2, img4, img5, img6, img7, img9, img10, img11, img13, img12, img15]; // Add more images as needed
+    const images = [img1, img2, img3, img4, img5, img6, img7, img9, img10, img11, img13, img15];
 
   
     return (
